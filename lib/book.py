69ad623d2fa16a33c813d3ba5a97938f84fd132a
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-08-10 16:13:48 +0300

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        # The setter is called implicitly here when you assign the value.
+        self.title = title
+        self.page_count = page_count
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+
+
+    @property
+    def page_count(self):
+        """Getter for page_count."""
+        return self._page_count
+
+   
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            # This 'print' statement matches the test's expectation
+            print("page_count must be an integer")
+        elif value <= 0:
+            print("page_count must be a positive number")
+        else:
+            self._page_count = value
+    
+
+   
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    def cobble(self):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+        
+    @property
+    def size(self):
+        # The getter must return the private attribute, self._size
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
